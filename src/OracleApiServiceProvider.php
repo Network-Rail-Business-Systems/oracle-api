047b2a280de09b118c48ef156ef28d014efdcd7f
Ambreen Jamal <ambreen.jamal@networkrail.co.uk>
2023-08-18 11:34:45 +0100

    rollback changes

--- src/OracleApiServiceProvider.php
+++ src/OracleApiServiceProvider.php
@@ -8,16 +8,11 @@ class OracleApiServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->mergeConfigFrom(
-            __DIR__ . '/oracle-catalogue.php',
-            'oracle-catalogue'
-        );
+      //
     }
 
     public function boot(): void
     {
-        $this->publishes([
-            __DIR__ . '/oracle-catalogue.php' => config_path('oracle-catalogue.php'),
-        ], 'oracle-catalogue');
+      //
     }
 }
